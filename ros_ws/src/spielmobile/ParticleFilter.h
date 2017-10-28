d730e7eda35539ecfc1e4a496223fd9bee931119
tommeredith14 <tommeredith14@gmail.com>
2017-10-28 17:05:43 -0400

    Saving

--- ros_ws/src/spielmobile/ParticleFilter.h
+++ ros_ws/src/spielmobile/ParticleFilter.h
@@ -14,39 +14,16 @@
 
 
 class CParticle {
-	private:
-		double x_pos;
-		double y_pos;
-		double heading;
 
 	public:
-		CParticle() {
+		CParticle();
+		CParticle(const CParticle& rhs);
 
-		}
-
-		void MotionUpdate(const geometry_msgs::Twist::ConstPtr& update) {
-			
-			if (update->linear.x != 0)
-			{
-				(*this).x_pos += update->linear.x * cos(heading);
-				(*this).y_pos += update->linear.x * sin(heading);
-			}
-			else if (update->angular.z > 0) {
-				double x_centre = (*this).x_pos - update->linear.z * sin((*this).heading);
-				double y_centre = (*this).y_pos + update->linear.z * cos((*this).heading);
-				(*this).x_pos = x_centre + update->linear.z * sin((*this).heading + update->angular.z);
-				(*this).y_pos = y_centre - update->linear.z * cos((*this).heading + update->angular.z);
-				(*this).heading += update->angular.z;
-			} else {
-				double x_centre = (*this).x_pos + update->linear.z * sin((*this).heading);
-				double y_centre = (*this).y_pos - update->linear.z * cos((*this).heading);
-				(*this).x_pos = x_centre - update->linear.z * sin((*this).heading - update->angular.z);
-				(*this).y_pos = y_centre + update->linear.z * cos((*this).heading - update->angular.z);
-				(*this).heading -= update->angular.z;
-			}
-
-
-		}
+	void MotionUpdate(const geometry_msgs::Twist::ConstPtr& update);
+private:
+	double x_pos;
+	double y_pos;
+	double heading;
 
 
 };
