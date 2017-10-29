1632e818e2de07b425d1bfa02c6231e09c9149ae
tommeredith14 <tommeredith14@gmail.com>
2017-10-29 12:37:48 -0400

    Saving

--- ros_ws/src/spielmobile/ParticleFilter.cpp
+++ ros_ws/src/spielmobile/ParticleFilter.cpp
@@ -27,15 +27,15 @@ void CParticle::MotionUpdate(const geometry_msgs::Twist::ConstPtr& update) {
 	}
 	else if (update->angular.z > 0) {
 		double x_centre = (*this).m_xpos - turnRad * sin((*this).heading);
-		double y_centre = (*this).m_ypos + update->linear.z * cos((*this).heading);
-		(*this).x_pos = x_centre + update->linear.z * sin((*this).heading + update->angular.z);
-		(*this).y_pos = y_centre - update->linear.z * cos((*this).heading + update->angular.z);
+		double y_centre = (*this).m_ypos + turnRad * cos((*this).heading);
+		(*this).x_pos = x_centre + turnRad * sin((*this).heading + update->angular.z);
+		(*this).y_pos = y_centre - turnRad * cos((*this).heading + update->angular.z);
 		(*this).heading += update->angular.z;
 	} else {
-		double x_centre = (*this).x_pos + update->linear.z * sin((*this).heading);
-		double y_centre = (*this).y_pos - update->linear.z * cos((*this).heading);
-		(*this).x_pos = x_centre - update->linear.z * sin((*this).heading - update->angular.z);
-		(*this).y_pos = y_centre + update->linear.z * cos((*this).heading - update->angular.z);
+		double x_centre = (*this).x_pos + turnRad * sin((*this).heading);
+		double y_centre = (*this).y_pos - turnRad * cos((*this).heading);
+		(*this).x_pos = x_centre - turnRad * sin((*this).heading - update->angular.z);
+		(*this).y_pos = y_centre + date->linear.z * cos((*this).heading - update->angular.z);
 		(*this).heading -= update->angular.z;
 	}
 
